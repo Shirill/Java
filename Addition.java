2deb27135201a5ee29f7035d33a00d0d28a07dd8
Shirill <74925859+Shirill@users.noreply.github.com>
2026-09-23 09:52:01 -0400

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int num1=6,num2=7,total;//Declaration and Assignment
+        total=num1+num2;
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("First Second");
+
+
+    }
+}
